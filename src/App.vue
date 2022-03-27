dc12c231d3c29d787e55c8e71c77f7908179ee4e
Evelin <teakid21@gmail.com>
2022-03-27 22:05:37 +0200

    Finish Vue.js scaffolding and the header in App.js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,169 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header>
+    <div class="logo">
+      <img src="./assets/logo.png" alt="Room Leaves logotype" height="48" >
+    </div>
+    <div class="right" id="right">
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+      <div class="header-icons">
+        <button @click="isSearchbarOpen = true; searchbarOpened()"><img height="20" src="./assets/search-icon.png" alt="Search Icon"></button>
+        <Transition name="widthAppearance">
+          <div v-show="isSearchbarOpen" id="searchbar">
+            <input name="search" type="text" placeholder="Search...">
+          </div>
+        </Transition>
+        <button><img height="20" src="./assets/shopping-cart-icon.png" alt="Search Icon"></button>
+      </div>
+    </div>
+  </header>
+  <router-view />
+  <footer>
+    <a href="https://www.flaticon.com/free-icons/cart" title="cart icons">Cart icons created by Catalin Fertu - Flaticon</a>
+    <a href="https://www.flaticon.com/free-icons/search" title="search icons">Search icons created by Catalin Fertu - Flaticon</a>
+  </footer>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isSearchbarOpen: false
+    }
+  },
+  methods: {
+    searchbarOpened() {
+      document.body.addEventListener("click", this.closeSearchBar.bind(event));
+    },
+    closeSearchBar(event) {
+      /* Hides the searchbar only in case you're clicking outside of the general box containing the nav bar and cart/search idons. */
+      if (!document.querySelector("#right").contains(event.target)) {
+        this.isSearchbarOpen = false;
+        document.body.removeEventListener("click", this.closeSearchBar);
+      }
+    }
+  }
+}
+
+</script>
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-family: 'Poppins', sans-serif;
 }
 
-#nav {
-  padding: 30px;
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  padding: 1rem 3rem;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
+
+.header-icons {
+  display: flex;
+  list-style: none;
+  gap: 1em;
+}
+
+.header-icons button {
+  cursor: pointer;
+  background: none;
+  border: none;
+  position: relative;
+}
+
+.header-icons button:hover::after {
+ content: "";
+  position: absolute;
+  background: #60894A;
+  height: 0.5rem;
+  width: 0.5rem;
+  top: -0.45rem;
+  border-radius: 50%;
+}
+
+.right {
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+  align-items: baseline;
+}
+
+nav {
+  display: flex;
+  gap: 1em;
+}
+
+nav a {
+  color: black;
+  text-decoration: none;
+  font-size: 1.1rem;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+
+#searchbar {
+  overflow: hidden;
+  width: 175px; /* Same as it's child, the input */
+}
+
+#searchbar input {
+  width: calc(100% - 4px); /* Same as it's parent */
+  border: none;
+  border-bottom: 1px solid rgba(0,0,0, 0.3);
+  font-size: 0.9rem;
+  margin: 2px;
+  padding: 0.25em 0 0.25em 0.75em;
+}
+
+.widthAppearance-enter-active,
+.widthAppearance-leave-active {
+  animation: showSearchbar 0.25s ease-out;
+}
+
+.widthAppearance-enter-from,
+.widthAppearance-leave-to {
+ animation: hideSearchbar 0.25s ease-out;
+}
+
+#searchbar input:focus {
+  outline: 2px solid rgba(16, 69, 71, 0.5);
+}
+
+@keyframes showSearchbar {
+  0% {
+    width: 0px;
+  }
+  100% {
+    width: 175px; /* Same as the container it's applied to --> #searchbar */
+  }
+}
+
+@keyframes hideSearchbar {
+  0% {
+    width: 175px; /* Same as the container it's applied to --> #searchbar */
+  }
+  100% {
+    width: 0px;
+  }
+}
+
+
 </style>
